556e4620f705f59708174b77bc42a21d04c5e645
GOLDENOMET <goldenomet0@gmail.com>
2025-05-30 10:52:47 +0100

    Fix dev script in package.json

--- client/src/components/GallerySection.tsx
+++ client/src/components/GallerySection.tsx
@@ -15,7 +15,7 @@ const GallerySection: React.FC = () => {
       alt: "Environmental assessment at industrial site"
     },
     {
-      src: "src/assets/SPEC logo.png",
+      src: "src/assets/SPEC EXAMPLE.jpg",
       alt: "Consultants analyzing environmental data"
     }
   ];
